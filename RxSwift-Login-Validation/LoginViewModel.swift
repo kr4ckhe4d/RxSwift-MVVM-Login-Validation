79f6a36308ef7ab480eec9b3fc6b0e268e145e36
Nipuna H Herath <nipuna@keeneye.solutions>
2017-07-12 09:54:39 +0530

    create demo login

---
+++ RxSwift-Login-Validation/LoginViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  LoginViewModel.swift
+//  RxSwift-Login-Validation
+//
+//  Created by Nipuna's MacMini on 7/12/17.
+//  Copyright © 2017 Nipuna H Herath. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+struct LoginViewModel {
+    var username = Variable<String>("")
+    var password = Variable<String>("")
+    
+    var isValid: Observable<Bool>{
+        return Observable.combineLatest(username.asObservable(),password.asObservable()){ email,password in
+            email.characters.count >= 3 && password.characters.count >= 3
+        }
+    }
+    
+}
--- RxSwift-Login-Validation/ViewController.swift
+++ RxSwift-Login-Validation/ViewController.swift
@@ -7,12 +7,32 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var validitylabel: UILabel!
+    @IBOutlet weak var loginbutton: UIButton!
 
+    var loginViewModel = LoginViewModel()
+    var disposeBag = DisposeBag()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        _ = username.rx.text.map{ $0 ?? "" }.bind(to: loginViewModel.username)
+        _ = password.rx.text.map{ $0 ?? "" }.bind(to: loginViewModel.password)
+        
+        _ = loginViewModel.isValid.bind(to: loginbutton.rx.isEnabled)
+        
+        _ = loginViewModel.isValid.subscribe(onNext: { [unowned self] isValid in
+            self.validitylabel.text = isValid ? "Valid" : "Form is Invalid"
+            self.validitylabel.textColor = isValid ? UIColor.green : UIColor.red
+        })
     }
 
     override func didReceiveMemoryWarning() {
